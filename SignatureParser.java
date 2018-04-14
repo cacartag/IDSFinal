5e28e24aeeebd6a03e01e8c088f1df5a4a3cee82
cacartag <cacartag@email.uark.edu>
2018-04-14 12:51:39 -0500

    completed parsing of initial rule format

--- SignatureParser.java
+++ SignatureParser.java
@@ -81,12 +81,12 @@ public class SignatureParser{
         }
         
         Signature temp = new Signature(acceptedProtocols);
-        temp.parse(rules[0]);
+        //temp.parse(rules[0]);
         
-//        for(int x = 0; x < rules.length; x++)
-//        {
-//            temp.parse(rules[x]);
-//        }
+        for(int x = 0; x < rules.length; x++)
+        {
+            temp.parse(rules[x]);
+        }
 
 
     }
@@ -183,23 +183,30 @@ class Signature{
             // parse and check ipsource and mask
             String[] splitIpMask = splitRule[2].split("/");
             
-            if(splitIpMask.length == 2)
+            if(splitIpMask.length == 2 || splitIpMask.length == 1)
             {
-                String[] ipSplit = splitIpMask[0].split("\\.");
-                
-                if(ipSplit.length == 4)
+                if(splitIpMask.length == 2)
                 {
+                    String[] ipSplit = splitIpMask[0].split("\\.");
+                    
+                    if(ipSplit.length == 4)
+                    {
+                        ipSource = splitIpMask[0];
+                    }
+                    
+                    maskSource = splitIpMask[1];
+                } else if(splitIpMask[0].equals("any")) {
+                    ipAnySource = true;
                     ipSource = splitIpMask[0];
-                    
-                    //for(int x = 0; x < ipSplit.length; x++)
-                    //    System.out.println(ipSplit[x]);
-                    
+                    maskSource = splitIpMask[0];
+                } else {
+                    System.out.println("Did not recognize ip source, and mask format");
                 }
-                
-                maskSource = splitIpMask[1];
                 
                 System.out.println("Ip Source: " + ipSource);
                 System.out.println("Mask Source: " + maskSource);
+                
+                
             }
             else {
                 System.out.println("Did not recognize ip source, and mask format");
@@ -207,11 +214,115 @@ class Signature{
             
             // parse and check the port source/sources
             String[] ports = splitRule[3].split(":");
+            if(ports.length == 1 || ports.length == 2)
+            {
+                port1Source = ports[0];
+
+                System.out.println("Port 1 source: " + port1Source);
+                
+                if(ports.length == 2)
+                {
+                    port2Source = ports[1];
+                    System.out.println("Port 2 source: " + port2Source);
+                }
             
+                if(port1Source.equals("any"))
+                {
+                    portAnySource = true;
+                    System.out.println("Source port any is set");
+                }
+            
+            } else {
+                System.out.println("Did not recognize the port format");
+            }
+            
+            // parse and check the direction
+            if(splitRule[4].equals("->") || splitRule[4].equals("<>"))
+            {
+                if(splitRule[4].equals("->"))
+                {
+                    bidirectional = false;
+                }
+                else{
+                    bidirectional = true;
+                }
+                
+                if(bidirectional)
+                {
+                    System.out.println("Direction: bidirectional");
+                } else{
+                    System.out.println("Direction: inbound");
+                }
+                    
+            } else {
+                System.out.println("Did not recognize the direction");
+            }
+            
+            // parse and check iptarget and mask
+            splitIpMask = splitRule[5].split("/");
+            
+            if(splitIpMask.length == 2)
+            {
+                if(splitIpMask.length == 2)
+                {
+                    String[] ipSplit = splitIpMask[0].split("\\.");
+                    
+                    if(ipSplit.length == 4)
+                    {
+                        ipTarget = splitIpMask[0];
+                    }
+                    
+                    maskTarget = splitIpMask[1];
+                } else if(splitIpMask[0].equals("any")) {
+                    ipAnyTarget = true;
+                    ipTarget = splitIpMask[0];
+                    maskTarget = splitIpMask[0];
+                } else {
+                    System.out.println("Did not recognize ip source, and mask format");
+                }
+                
+                System.out.println("Ip Target: " + ipTarget);
+                System.out.println("Mask Target: " + maskTarget);
+            }
+            else {
+                System.out.println("Did not recognize ip source, and mask format");
+            }
+            
+            // parse and check the port target/targets
+            ports = splitRule[6].split(":");
+            if(ports.length == 1 || ports.length == 2)
+            {
+                port1Target = ports[0];
+
+                System.out.println("Port 1 target: " + port1Target);
+                
+                if(ports.length == 2)
+                {
+                    port2Target = ports[1];
+                    System.out.println("Port 2 target: " + port2Target);
+                }
+                
+                if(port1Target.equals("any"))
+                {
+                    portAnySource = true;
+                    System.out.println("Target port any is set");
+                }
+            
+            } else {
+                System.out.println("Did not recognize the port format");
+            }
+            
+            if(splitRule.length > 7)
+            {
+                System.out.println("Options detected");
+            }
         } else {
-            System.out.println("Rule is missing arguments");
+            System.out.println("Rule is missing arguments, recheck format");
         }
         
+        System.out.println();
+        System.out.println();
+
         
         //for(int x = 0; x < 7; x++)
         //{
